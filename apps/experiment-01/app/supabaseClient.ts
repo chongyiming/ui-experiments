e596c65a440e9a12c8391be0abcc8f48135ff6d5
chong yi ming <chongyiming1205@gmail.com>
2025-03-22 02:57:02 +0800

    Make test branch empty

--- apps/experiment-01/app/supabaseClient.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://velfmvmemrzurdweumyo.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZlbGZtdm1lbXJ6dXJkd2V1bXlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM2NTc2ODUsImV4cCI6MjA0OTIzMzY4NX0.wYS5iKIce0cf_yxnh8XGOYmhl0xW8gND5abvYRZEU1o";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
